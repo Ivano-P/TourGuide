083c85d4c37e2727815f7a3f0342fafa18a59af8
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2023-09-27 12:35:03 +0200

    updated architecture, added controller to it's own package and created module

--- TourGuide/src/main/java/com/openclassrooms/tourguide/modules/TourGuideModule.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/modules/TourGuideModule.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.tourguide;
+package com.openclassrooms.tourguide.modules;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
